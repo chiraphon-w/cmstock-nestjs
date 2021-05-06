5b128fa62c5f5c6a6d04a289b3797102177f30f6
chiraphon-w <chw242542@gmail.com>
2021-05-07 01:41:08 +0700

    Part1-EP8 Complete Stock Routing Definition

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,5 +1,16 @@
 import { CreateStockDto } from './dto/create-stock-dto';
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('stock') //path 'stock'
 export class StockController {
@@ -17,7 +28,31 @@ export class StockController {
   @UsePipes(ValidationPipe) // ใช้เพื่อตรวจสอบว่าข้อมูลที่ส่งมาผ่าน dto ครบถ้วนไหม
   addStock(@Body() createStockDto: CreateStockDto) {
     const { name, price, stock } = createStockDto;
-    
     console.log(`name: ${name}, price: ${price}, stock: ${stock}`);
+  }
+
+  @Get('/:id') //ยิง id เข้ามา
+  getStockById(@Param('id') id: number) {
+    //Param คือการ map id
+    return `Get id is ${id}`;
+  }
+
+  @Delete('/:id') //ยิง id เข้ามา
+  deleteStockById(@Param('id') id: number) {
+    //Param คือการ map id
+    return `Delete id is ${id}`;
+  }
+
+  //put ยิงข้อมูลทั้งหมด
+  //patch ยิงเฉพาะส่วนที่ต้องการ
+  @Put('/:id') 
+  UpdateStockById(
+    @Param('id') id: number, //param or body ก็ได้ แต่จะเรียกผ่าน Body ได้ก็ต่อเมื่อมีการประกาศตัวแปรหรือ field ใน Dto เท่านั้น
+    @Body() createStockDto: CreateStockDto,
+  ) {
+    const { name, price, stock } = createStockDto;
+    console.log(`name: ${name}, price: ${price}, stock: ${stock}`);
+
+    return `Update id is ${id}, ${name}, ${price}, ${stock}`;
   }
 }
