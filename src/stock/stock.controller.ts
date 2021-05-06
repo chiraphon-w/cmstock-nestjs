486c829a88a725bafc7fcaa1f21ec6f58c82be02
chiraphon-w <chw242542@gmail.com>
2021-05-07 00:53:22 +0700

    Part1-EP5 AddStock Post

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('stock') //path 'stock'
 export class StockController {
   @Get()
   getStocks() {
     return [1, 2, 3];
+  }
+
+  @Post()
+  addStock(@Body('name') name: string, @Body('price') price: number) {
+    console.log(`name: ${name}, price: ${price}`);
   }
 }
