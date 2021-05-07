5076b189d852cf17e38bc3ce40d1d9a492125457
chiraphon-w <chw242542@gmail.com>
2021-05-07 15:23:27 +0700

    Part2-EP15 TypeOrm Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StockModule } from './stock/stock.module';
+import { typeOrmConfig } from './config/typeorm.config';
+
 //กลุ่มของ function
 @Module({
-  imports: [StockModule],
+  imports: [StockModule, TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
