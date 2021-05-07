ddb789df7bd9cbc6c6752ed5399499e1a47908e0
chiraphon-w <chw242542@gmail.com>
2021-05-07 16:27:03 +0700

    Part2-EP17 comment

--- src/stock/product.repository.ts
+++ src/stock/product.repository.ts
@@ -2,5 +2,7 @@ import { Product } from './product.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 // 1 repo = 1 table
+// repo เป็นคนคุยกับ db ว่าจะเอาข้อมูลตรงไหนบ้าง แล้วส่งต่อให้ service
+
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {}
