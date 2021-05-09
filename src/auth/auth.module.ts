576b2aef018b200b3ad39e5961ba07af0e06e35b
chiraphon-w <chw242542@gmail.com>
2021-05-09 22:48:59 +0700

    Part7-EP41 Register JWT and Passport Library

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'chira123', // รหัสลับที่ใช้ในการ endcode
+      signOptions: {
+        expiresIn: 5000,
+      },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
